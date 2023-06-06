069e7e09bdc387df786cd64fe1ac514a60fc84d4
Clourc <corbino.lucas@gmail.com>
2023-06-06 11:41:50 +0200

    Before dev pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DetailComponent } from './detail/detail.component';
-import {OverlayModule} from '@angular/cdk/overlay';
+import { OverlayModule } from '@angular/cdk/overlay';
 import { GameCardComponent } from './game-card/game-card.component';
 import { GameListComponent } from './game-list/game-list.component';
-
-
-
-
 
 @NgModule({
   declarations: [
@@ -19,18 +15,9 @@ import { GameListComponent } from './game-list/game-list.component';
     DetailComponent,
     GameCardComponent,
     GameListComponent,
-    
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    OverlayModule,
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, OverlayModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
-
-
+export class AppModule {}
