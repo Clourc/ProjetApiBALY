cefa9caabe1c6726929271090e93629c4951bda6
darenik67 <karma-67.75@hotmail.fr>
2023-06-09 11:49:11 +0200

    random button modifications

--- src/app/api-config/config.ts
+++ src/app/api-config/config.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export const callAPI = function (
+export  const callAPI = function (
   http: HttpClient,
   endpoint: string
 ): Observable<any[]> {
