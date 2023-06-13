aa3014ba29a71534837df174aa82f246e9e471bb
Clourc <corbino.lucas@gmail.com>
2023-06-13 10:49:30 +0200

    Added specific routing for game list, added app random to game list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { GameListComponent } from './game-list/game-list.component';
 import { DetailComponent } from './detail/detail.component';
 import { SearchComponent } from './search/search.component';
-import { HeaderComponent } from './header/header.component';
-import { CardFavComponent } from './card-fav/card-fav.component';
 import { MentionsComponent } from './mentions/mentions.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
@@ -12,14 +10,16 @@ import { RandomComponent } from './random/random.component';
 
 
 const routes: Routes = [
-  { path: '', component: GameListComponent},
+  {path : 'games', component: GameListComponent},
   { path: 'game/:id', component: DetailComponent},
   { path:'search', component: SearchComponent},
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent},
   { path: 'mentions', component: MentionsComponent},
-  { path: 'random', component: RandomComponent}
-
+  { path: 'random', component: RandomComponent},
+  { path: '', redirectTo: 'games', pathMatch: 'full'},
+/*   { path: '**', component: GameListComponent}
+ */
 ];
 
 @NgModule({
