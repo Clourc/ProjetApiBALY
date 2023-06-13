727e46e2cd4733537077f29edbff2a11f886c6ac
Clourc <corbino.lucas@gmail.com>
2023-06-13 12:40:22 +0200

    Added thumbnail roulette for random. Last thumbnail is of game randomly selected

--- src/app/random/random.component.ts
+++ src/app/random/random.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { callAPI } from '../api-config/config';
@@ -7,39 +7,60 @@ import { getMaxId } from '../functions/functions';
 @Component({
   selector: 'app-random',
   templateUrl: './random.component.html',
-  styleUrls: ['./random.component.css']
+  styleUrls: ['./random.component.css'],
 })
 export class RandomComponent {
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
   gameList: any[] = [];
   maxId!: number;
+  randomGame: any;
+  randomThumbnail: string = '';
 
   ngOnInit() {
     callAPI(this.http, 'games').subscribe((data) => {
       this.maxId = getMaxId(data);
-      this.gameList = data
-      console.log(data)
-    })
+      this.gameList = data;
+      console.log(this.gameList);
+    });
   }
 
+  
+  displayRandomThumbnail() {
+    const randomArrayStart = Math.floor(Math.random() * this.gameList.length);
+    let randomArrayEnd: number;
+    let i = 0;
+    
+    if (randomArrayStart < this.gameList.length - 21) {
+      randomArrayEnd = randomArrayStart + 20;
+    } else {
+      randomArrayEnd = randomArrayStart - 20;
+    }
+    
+    const arrayPart = this.gameList.slice(randomArrayStart, randomArrayEnd);
+    this.getRandomGame(this.maxId);
+    arrayPart.push(this.randomGame);
+    console.log(arrayPart);
+    while(i < arrayPart.length) {
+      this.thumbnailLoop(arrayPart, i);
+      i++
+    }
+  }
+  
   getRandomGame(maxId: any) {
     let goRandom = Math.floor(Math.random() * (maxId - 1) + 1);
     console.log(goRandom);
-    if(this.gameList.some(game => game.id === goRandom)) {
-      this.router.navigate([`game/${goRandom}`]);
+    if (this.gameList.some((game) => game.id === goRandom)) {
+      this.randomGame = this.gameList.find((game) => game.id === goRandom);
+      console.log(this.randomGame);
     } else {
       this.getRandomGame(this.maxId);
     }
   }
 
-  sortGameIds(array: any[]) {
-    array.sort((element1: any, element2: any) => {
-      return element1.id - element2.id
-    })
-    console.log(array)
-  }
-
-  ifGameExist(http: HttpClient, id: number) {
-    return callAPI(http, `game/${id}`);
+  thumbnailLoop(array: any[], i: number){
+    setTimeout(() => {
+      this.randomThumbnail = array[i].thumbnail;
+      console.log(this.randomThumbnail);
+    }, 300 * i);
   }
 }
